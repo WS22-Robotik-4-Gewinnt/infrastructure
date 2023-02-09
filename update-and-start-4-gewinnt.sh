6f9e8e0de68ce56f5080e9d8342971cf5a340d6b
Keksbendiger <Keksbendiger@users.noreply.github.com>
2023-02-09 10:21:17 +0100

    Update update-and-start-4-gewinnt.sh

--- update-and-start-4-gewinnt.sh
+++ update-and-start-4-gewinnt.sh
@@ -1,16 +1,9 @@
 #! /bin/bash
 
-mkdir Bildverarbeitung
-cd ./Bildverarbeitung
 git clone https://github.com/WS22-Robotik-4-Gewinnt/Bildverarbeitung.git
 
-mkdir ../Spielalgorithmus
-cd ../Spielalgorithmus
 git clone https://github.com/WS22-Robotik-4-Gewinnt/Spielalgorithmus.git
 
-mkdir ../Hardwaresteuerung
-cd ../Hardwaresteuerung
 git clone https://github.com/WS22-Robotik-4-Gewinnt/Hardwaresteuerung.git
 
-cd ..
 docker compose up -d --build --force-recreate 
